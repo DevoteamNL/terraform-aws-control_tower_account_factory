85da36a74a063c6c94b7e74d2b1fc5281377baad
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-03-25 17:28:52 +0100

    feat: update and pin aft module to 1.12.0

--- examples/github+tf_oss/main.tf
+++ examples/github+tf_oss/main.tf
@@ -2,18 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory?ref=1.12.0"
   # Required Vars
-  ct_management_account_id    = "111122223333"
-  log_archive_account_id      = "444455556666"
-  audit_account_id            = "123456789012"
-  aft_management_account_id   = "777788889999"
-  ct_home_region              = "us-east-1"
-  tf_backend_secondary_region = "us-west-2"
+  ct_management_account_id    = "316689970579"
+  log_archive_account_id      = "903296247028"
+  audit_account_id            = "764660151821"
+  aft_management_account_id   = "891377100111"
+  ct_home_region              = "eu-west-1"
+  tf_backend_secondary_region = "eu-central-1"
   # VCS Vars
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "ExampleOrg/example-repo-1"
-  global_customizations_repo_name               = "ExampleOrg/example-repo-2"
-  account_customizations_repo_name              = "ExampleOrg/example-repo-3"
-  account_provisioning_customizations_repo_name = "ExampleOrg/example-repo-4"
+  account_request_repo_name                     = "DevoteamNL/aft-account-request"
+  global_customizations_repo_name               = "DevoteamNL/aft-global-customizations"
+  account_customizations_repo_name              = "DevoteamNL/aft-account-customizations"
+  account_provisioning_customizations_repo_name = "DevoteamNL/aft-provisioning-customizations"
 }
+
